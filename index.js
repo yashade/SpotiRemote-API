6314a6092558caa3638c6493df2cf867327ddf91
Yashar Ahmedizade <yashade2001@gmail.com>
2016-09-26 23:40:51 +0300

    oops
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- index.js
+++ index.js
@@ -2,30 +2,26 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function(socket) {
+app.post('/api/next', function (req, res) {
+  io.emit('next');
+  res.sendStatus(200);
+})
 
-  app.post('/api/next', function (req, res) {
-    io.emit('next');
-    res.sendStatus(200)
-  })
+app.post('/api/playpause', function (req, res) {
+  io.emit('playpause');
+  res.sendStatus(200);
+})
 
-  app.post('/api/playpause', function (req, res) {
-    io.emit('playpause');
-    res.sendStatus(200)
-  })
+app.post('/api/prev', function (req, res) {
+  io.emit('prev');
+  res.sendStatus(200);
+})
 
-  app.post('/api/prev', function (req, res) {
-    io.emit('prev');
-    res.sendStatus(200)
-  })
-
-  app.post('/api/searchplay', function (req, res) {
-    io.emit('searchplay', {
-      query: req.query.query
-    });
-    res.sendStatus(200)
-  })
-
-});
+app.post('/api/searchplay', function (req, res) {
+  io.emit('searchplay', {
+    query: req.query.query
+  });
+  res.sendStatus(200);
+})
 
 http.listen(1337);
