5006e9fd1958e011ac0cb1a34009027d31509a8e
Yashar Ahmedizade <yashade2001@gmail.com>
2016-09-26 20:25:38 +0300

    remove web client
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- index.js
+++ index.js
@@ -2,30 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket) {
-  socket.on('internal_next', function() {
-    io.emit('next');
-  });
 
-  socket.on('internal_playpause', function() {
-    io.emit('playpause');
-  });
-
-  socket.on('internal_prev', function() {
-    io.emit('prev');
-  });
-
-  socket.on('internal_searchplay', function(data){
-    io.emit('searchplay', {
-      query: data
-    });
-  });
-
-  // REST API
   app.post('/api/next', function (req, res) {
     io.emit('next');
     res.sendStatus(200)
