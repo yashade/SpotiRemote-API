5f4d0168f7d75cc02d36f82bb4d34f09869d3a40
Yashar Ahmedizade <yashade2001@gmail.com>
2016-09-27 22:53:34 +0300

    volume api
    Signed-off-by: Yashar Ahmedizade <yashade2001@gmail.com>

--- index.js
+++ index.js
@@ -5,23 +5,30 @@ var io = require('socket.io')(http);
 app.post('/api/next', function (req, res) {
   io.emit('next');
   res.sendStatus(200);
-})
+});
 
 app.post('/api/playpause', function (req, res) {
   io.emit('playpause');
   res.sendStatus(200);
-})
+});
 
 app.post('/api/prev', function (req, res) {
   io.emit('prev');
   res.sendStatus(200);
-})
+});
 
 app.post('/api/searchplay', function (req, res) {
   io.emit('searchplay', {
     query: req.query.query
   });
   res.sendStatus(200);
-})
+});
+
+app.post('/api/setvolume', function (req, res) {
+  io.emit('setvolume', {
+    level: req.query.level
+  });
+  res.sendStatus(200);
+});
 
 http.listen(1337);
